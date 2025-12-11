08652a32743f228d8995a1fe5898da6add0553a5
Mateusz <mateuszjens14@gmail.com>
2025-12-11 10:50:11 +0100

    tydzien 1

---
+++ models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class MuscleGroup(models.TextChoices):
+    CHEST = 'CHEST', 'Chest'
+    BACK = 'BACK', 'Back'
+    LEGS = 'LEGS', 'Legs'
+    CORE = 'CORE', 'Core'
+
+class DifficultyLevel(models.TextChoices):
+    BEGINNER = 'BEGINNER', 'Beginner'
+    INTERMEDIATE = 'INTERMEDIATE', 'Intermediate'
+    ADVANCED = 'ADVANCED', 'Advanced'
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=100)
+    muscle_group = models.CharField(
+        max_length=20,
+        choices=MuscleGroup.choices
+    )
+    difficulty = models.CharField(
+        max_length=20,
+        choices=DifficultyLevel.choices
+    )
